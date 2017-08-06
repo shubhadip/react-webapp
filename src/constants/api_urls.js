cb4db3bb80421c7483645e29182ed9437694d1ac
shubhadip maity <shubhadipmaity@mighty.local>
2017-08-06 08:01:52 +0530

    url added

--- src/constants/api_urls.js
+++ src/constants/api_urls.js
@@ -1,7 +1,7 @@
-export const API_URL = 'http://localhost:3000/v1/admin';
+export const URL = 'http://localhost:3000/v1/admin';
 export const STAGING_URL = 'https://staging.yyyyy.com/v1/admin';
 export const LIVE_URL = 'http://test.yyyyy.com/v1/admin';
-
+export const API_URL = STAGING_URL;
 export const AUTHENTICATION_URL = '/authentication';
 export const ADMIN_LIST_URL = '/admin_users?fields=first_name,last_name,designation,department,enable,can_login,can_sales_login,employee_code,email&limit=500';
 export const ADMIN_UPDATE_URL = '/admin_users';
